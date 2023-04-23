b7049980eff04fc0853dc165a7916e74568b1f3c
diana1m <diana.maliarchuk.work@gmail.com>
2023-04-23 09:01:08 +0000

    Deploying to gh-pages from @ diana1m/test-tweet-cards@6c9d7e9f6a683a0ceee3568ad97a1db2b3589977 🚀

--- static/js/522.06c05256.chunk.js
+++
@@ -1,2 +0,0 @@
-"use strict";(self.webpackChunktest_tweet_cards=self.webpackChunktest_tweet_cards||[]).push([[522],{522:function(e,t,n){n.r(t),n.d(t,{default:function(){return r}});var s=n(184);function r(){return(0,s.jsx)("h2",{children:"Tweets page"})}}}]);
-//# sourceMappingURL=522.06c05256.chunk.js.map
