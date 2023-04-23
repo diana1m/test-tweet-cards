1183586116257ef81c43b1855f2adf4c61945478
diana1m <diana.maliarchuk.work@gmail.com>
2023-04-23 11:38:17 +0000

    Deploying to gh-pages from @ diana1m/test-tweet-cards@c509aff968df97356faea93697362583367ba048 🚀

--- static/js/266.a706124f.chunk.js
+++
@@ -1,2 +0,0 @@
-"use strict";(self.webpackChunktest_tweet_cards=self.webpackChunktest_tweet_cards||[]).push([[266],{266:function(e,t,n){n.r(t),n.d(t,{default:function(){return s}});var r=n(184);function s(){return(0,r.jsx)("h1",{children:"Home page"})}}}]);
-//# sourceMappingURL=266.a706124f.chunk.js.map
